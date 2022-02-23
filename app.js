e42685b8495f35c4a402097a9313741d35ac4816
Nasa28 <kaluchinasaagu@gmail.com>
2022-02-24 00:39:21 +0100

    Add app.js

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+
+// Set security HTTP headers using helmet package
+
+app.use(express.json({ limit: '10kb' }));
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+
+
+// app.use('/api/v1/hotels', hotelRoutes);
+
+
+// app.all('*', (req, res, next) => {
+//   next(new AppError(`Can't find this ${req.originalUrl} on this server`, 400));
+// });
+// app.use(globalErrorHandler);
+
+module.exports = app;
